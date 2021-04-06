4e46ba8ccf43177d18a2739f31773f729f50f178
mama00 <mixture050@gmail.com>
2021-04-06 12:26:25 -0400

    schema

--- kernel/paging/bitmap.h
+++ kernel/paging/bitmap.h
@@ -7,6 +7,7 @@
  void set_frame(uint32_t frame_addr);
  void clear_frame(uint32_t frame_addr);
  uint32_t test_frame(uint32_t frame_addr);
+uint32_t first_frame();
 
 uint32_t INDEX_FROM_BIT( uint32_t a);
 uint32_t OFFSET_FROM_BIT(uint32_t a);
